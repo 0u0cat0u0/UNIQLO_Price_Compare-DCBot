1258547485ae7fbbf18d638694c5df948be9f9ad
unknown <you@example.com>
2024-12-30 02:30:25 +0800

    做一堆

--- UNIQLO_Price_Compare.py
+++ UNIQLO_Price_Compare.py
@@ -1,7 +1,14 @@
+#import discord
 import requests
-import discord
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import re
 
-intents = discord.Intents.all()
+"""intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 @client.event
@@ -10,36 +17,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    userMessage = message.content #使用者輸入的訊息
+
     if message.author == client.user:
         return
 
     #幫助
-    if message.content == 'help':
-        await message.channel.send('買 標的 數量 價格\n'+
-                                   '賣 標的 數量 價格\n'+
-                                   '套利 usdt twd\n'+
-                                   'BITO usdt twd\n'+
-                                   'MAX usdt twd\n'+
-                                   'EX usd twd\n'+
-                                   'US tsm\n'+
-                                   'TW 2330')
+    if re.compile('!help',re.IGNORECASE).match(userMessage) or re.compile('!h').match(userMessage): #re.IGNORECASE忽略大小寫
+        await message.channel.send('輸入商品代號或貼上商品網址')
 
-#----------查詢價格----------
-
-    #BITO MAX 套利
-    elif message.content.startswith('AR') or message.content.startswith('ar') or message.content.startswith('套利'):
-        tmp = message.content.split(' ')
-        url_BITO = 'https://api.bitopro.com/v3/tickers/'
-        url_MAX = 'https://max-api.maicoin.com/api/v2/tickers/'
-        html_BITO = requests.get(url_BITO+tmp[1]+'_'+tmp[2])
-        html_MAX = requests.get(url_MAX+tmp[1]+tmp[2])
-
-        await message.channel.send(arbitrage(tmp[2],tmp[1],html_BITO,html_MAX))
+    elif re.compile(r'\\d{6}').match(userMessage):
+        await message.channel.send(searchProductPrice)
 
 #----------子函式----------
 
 #交易所套利
-def arbitrage(currency,symbol,html_BITO,html_MAX):
+async def arbitrage(currency,symbol,html_BITO,html_MAX):
     data_BITO = html_BITO.json()
     data_MAX = html_MAX.json()
     price_BITO = data_BITO['data']['lastPrice']
@@ -50,5 +43,75 @@ def arbitrage(currency,symbol,html_BITO,html_MAX):
 
     return (str(symbol).upper()+' 對 '+str(currency).upper()+' 交易對的價差(Bito pro/Max)為 '+str(round(per,3))+' % ,最小投資金額為 $ '+str(money)+' '+str(currency).upper())
 
+async def searchProductPrice(productID):
+    URL = 'https://www.uniqlo.com/tw/zh_TW/search.html?description=' + productID.to_string()
+    headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+    HTML = requests.get(URL, headers=headers)
 
-client.run('') 
+    try:
+        data = HTML.json()
+        price = data['price']
+        return price
+    except:
+        return '找不到此商品'
+
+client.run('') """
+
+#用商品ID查詢台灣價格，回傳int
+def searchTWProductPrice(productID):
+    url = 'https://www.uniqlo.com/tw/zh_TW/search.html?description=' + productID
+    options = Options()
+    options.headless = True    #設定為True可以在後台運行Chrome
+    service = Service(ChromeDriverManager().install())  #安裝ChromeDriver
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.implicitly_wait(30)  #等待30秒，讓網頁載入
+    driver.get(url)
+    price = driver.find_element(By.CLASS_NAME, 'h-currency.bold').text  #用selenium套件查詢特定class，找到價格
+    driver.quit()   #關閉瀏覽器
+    return int(re.sub(r'\D', '', price))    #用re.sub去掉價格中的非數字部分
+
+#用商品ID查詢日本價格，換匯成台幣，回傳int
+def searchJPProductPrice(productID):
+    url = 'https://www.uniqlo.com/jp/ja/search?q=' + productID
+    options = Options()
+    options.headless = True
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service = service, options = options)
+    driver.implicitly_wait(30)
+    driver.get(url)
+    price = driver.find_element(By.CLASS_NAME, 'fr-ec-price-text.fr-ec-price-text--middle.fr-ec-price-text--color-primary-dark.fr-ec-text-transform-normal').text
+    driver.quit()
+    return searchExchangeRate(int(re.sub(r'\D', '', price)))
+
+#查詢匯率，回傳int
+def searchExchangeRate(productPrice):
+    currencyToChange = 'JPY'   #要換的貨幣
+    currencyChangeTo = 'TWD'   #要換成的貨幣
+    #呼叫Yahoo Finance API查詢匯率
+    url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + currencyToChange + currencyChangeTo + '=X?includePrePost=false&interval=1d&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
+    headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+    html = requests.get(url, headers=headers)
+    data = html.json()
+    exchangeRate = data['chart']['result'][0]['meta']['regularMarketPrice']
+    return int(float(productPrice) * float(exchangeRate))
+
+#查詢台灣價格和日本價格並回傳，呼叫查詢台灣價格、日本價格，最後呼叫查詢匯率，把日幣換算成台幣
+def priceCompare(productID):
+    twPrice = searchTWProductPrice(productID)
+    jpPrice = searchJPProductPrice(productID)
+    twURL = 'https://www.uniqlo.com/tw/zh_TW/search.html?description=' + productID
+    jpURL = 'https://www.uniqlo.com/jp/ja/search?q=' + productID
+    comparison = '台灣價格：' + str(twPrice) + ', 日本價格：' + str(jpPrice) + ', 價差：' + str(abs(twPrice - jpPrice))
+
+    #比較台灣價格和日本價格，如果日本價格比較低，回傳日本網址，反之回傳台灣網址
+    if twPrice < jpPrice:
+        comparison += '\n台灣UNIQLO網址：\n' + twURL
+    else:
+        comparison += '\n日本UNIQLO網址：\n' + jpURL
+    return comparison
+
+def main():
+    print(priceCompare('461011'))
+
+if __name__ == '__main__':
+    main()
