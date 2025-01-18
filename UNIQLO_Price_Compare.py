90ab279c30e4923eb2a77cc3d1967757b4d68b6b
unknown <you@example.com>
2025-01-18 17:42:16 +0800

    把selenium改成POST跟GET，新增把ID轉換成URL的函式

--- UNIQLO_Price_Compare.py
+++ UNIQLO_Price_Compare.py
@@ -1,11 +1,5 @@
 import discord
 import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import re
 
 DISCORDTOKEN = ''
@@ -40,77 +34,84 @@ async def on_message(message):
 
 #查詢台灣價格和日本價格並回傳，呼叫查詢台灣價格、日本價格，最後呼叫查詢匯率，把日幣換算成台幣
 def priceCompareID(productID):
+    jpPrice = searchJPProductPrice(productID)
+    #如果找不到商品就直接結束搜尋
+    if jpPrice == -1:
+        return '日本UNIQLO找不到此商品'
+
     twPrice = searchTWProductPrice(productID)
     if twPrice == -1:
         return '台灣UNIQLO找不到此商品'
     
-    jpPrice = searchJPProductPrice(productID)
-    if jpPrice == -1:
-        return '日本UNIQLO找不到此商品'
-
-    twURL = 'https://www.uniqlo.com/tw/zh_TW/search.html?description=' + productID
-    jpURL = 'https://www.uniqlo.com/jp/ja/search?q=' + productID
-    comparison = '台灣價格：' + str(twPrice) + ', 日本價格：' + str(jpPrice) + ', 價差：' + str(abs(twPrice - jpPrice))
+    jpURL = convertJPIDToURL(productID)
+    twURL = convertTWIDToURL(productID)
+    comparison = '台灣價格：' + str(twPrice) + '\n日本價格：' + str(jpPrice) + '\n價差：' + str(abs(twPrice - jpPrice))
 
     #比較台灣價格和日本價格，如果日本價格比較低，回傳日本網址，反之回傳台灣網址
-    if twPrice < jpPrice:
-        comparison += '\n台灣UNIQLO網址：\n' + twURL
-    else:
+    if jpPrice < twPrice:
         comparison += '\n日本UNIQLO網址：\n' + jpURL
+    else:
+        comparison += '\n台灣UNIQLO網址：\n' + twURL
     return comparison
 
-#查詢台灣價格和日本價格並回傳，呼叫查詢台灣價格或日本價格，如果式日幣就查詢匯率，把日幣換算成台幣
-def priceCompareURL(url):
-    if re.match(r'https://www\.uniqlo\.com/tw/zh_TW/product-detail\.html\?productCode=.*', url):
-        productID = convertTWURL(url)
+#查詢台灣價格和日本價格並回傳，呼叫查詢台灣價格或日本價格，如果是日幣就查詢匯率，把日幣換算成台幣
+def priceCompareURL(productURL):
+    if re.match(r'https://www\.uniqlo\.com/tw/zh_TW/product-detail\.html\?productCode=.*', productURL):
+        productID = convertTWURLToID(productURL)
         price = searchJPProductPrice(productID)
-        productURL = 'https://www.uniqlo.com/jp/ja/search?q=' + productID
+        productURL = convertJPIDToURL(productID)
         comparision = '日本'
-    elif re.match(r'https://www\.uniqlo\.com/jp/ja/products/E.*', url):
-        productID = convertJPURL(url)
+    elif re.match(r'https://www\.uniqlo\.com/jp/ja/products/E.*', productURL):
+        productID = convertJPURLToID(productURL)
         price = searchTWProductPrice(productID)
-        productURL = 'https://www.uniqlo.com/tw/zh_TW/search.html?description=' + productID
+        productURL = convertTWIDToURL(productID)
         comparision = '台灣'
 
     if price == -1:
-        comparision += 'UNIQLO無此商品'
+        comparision += 'UNIQLO找不到此商品'
     else:
         comparision += '價格：' + str(price) + '\n商品網址：\n' + productURL
     return comparision
 
-#用商品ID查詢台灣價格，回傳int，回傳int，如果找不到商品回傳-1
+#用商品ID查詢台灣價格，回傳int，回傳int，如果找不到商品回傳-1，在需要查詢台灣UNIQLO價格時使用
 def searchTWProductPrice(productID):
     try:
-        url = 'https://www.uniqlo.com/tw/zh_TW/search.html?description=' + productID
-        options = Options()
-        options.headless = True    #設定為True可以在後台運行Chrome
-        service = Service(ChromeDriverManager().install())  #安裝ChromeDriver
-        driver = webdriver.Chrome(service=service, options=options)
-        driver.implicitly_wait(10)  #等待10秒，讓網頁載入
-        driver.get(url)
-        price = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/div/div/div/div/div[5]/div/div[1]/ul/li/div/a/div/div[2]/div[2]/div[3]/span').text  #用selenium套件查詢特定class，找到價格
-        driver.quit()   #關閉瀏覽器
-        return int(re.sub(r'\D', '', price))    #用re.sub去掉價格中的非數字部分
+        url = 'https://d.uniqlo.com/tw/p/search/products/by-description'
+        headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+        json = {
+            'url':'/tw/zh_TW/search.html?description=' + productID,
+            'pageInfo':{'page':1,'pageSize':24},
+            'belongTo':'pc',
+            'rank':'overall',
+            'priceRange':{'low':0,'high':0},
+            'color':[],
+            'size':[],
+            'identity':[],
+            'exist':[],
+            'searchFlag':True,
+            'description':productID,
+            'stockFilter':'warehouse'
+        }
+        html = requests.post(url, headers=headers, json=json, timeout=10)
+        data = html.json()
+        price = data['resp'][0]['productList'][0]['prices'][0]
+        return int(price)    #用re.sub去掉價格中的非數字部分
     except:
         return -1   #找不到商品回傳-1
 
-#用商品ID查詢日本價格，換匯成台幣，回傳int，如果找不到商品回傳-1
+#用商品ID查詢日本價格，換匯成台幣，回傳int，如果找不到商品回傳-1，在需要查詢日本UNIQLO價格時使用    
 def searchJPProductPrice(productID):
     try:
-        url = 'https://www.uniqlo.com/jp/ja/search?q=' + productID
-        options = Options()
-        options.headless = True
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service = service, options = options)
-        driver.implicitly_wait(10)
-        driver.get(url)
-        price = driver.find_element(By.XPATH, '/html/body/div[1]/section[1]/div[2]/section/div/div[2]/div/div[1]/div[1]/div/div/a/div[2]/div[2]/div/div[1]/div/p').text
-        driver.quit()
-        return searchExchangeRate(int(re.sub(r'\D', '', price)))
+        url = 'https://www.uniqlo.com/jp/api/commerce/v5/ja/products?productIds=E' + productID + '-000'
+        headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+        html = requests.get(url, headers=headers, timeout=10)
+        data = html.json()
+        price = data['result']['items'][0]['prices']['base']['value']
+        return searchExchangeRate(int(price))
     except:
         return -1
 
-#查詢匯率，回傳int
+#查詢匯率，回傳int，在需要把日幣換算成台幣時使用
 def searchExchangeRate(productPrice):
     currencyToChange = 'JPY'   #要換的貨幣
     currencyChangeTo = 'TWD'   #要換成的貨幣
@@ -122,21 +123,49 @@ def searchExchangeRate(productPrice):
     exchangeRate = data['chart']['result'][0]['meta']['regularMarketPrice']
     return int(float(productPrice) * float(exchangeRate))
 
-#把台灣商品網址轉換成商品ID，回傳str
-def convertTWURL(url):
-    options = Options()
-    options.headless = True
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service = service, options = options)
-    driver.implicitly_wait(10)
-    driver.get(url)
-    productID = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/div/div/div/div/div[1]/div[4]/div[2]/div/div[1]/div[1]').text
-    driver.quit()
+#把台灣商品網址轉換成商品ID，回傳str，在輸入台灣商品網址時呼叫
+def convertTWURLToID(productURL):
+    productCode = re.sub(r'\D', '', productURL)     #用re.sub去掉非數字部分
+    url = "https://d.uniqlo.com/tw/p/product/detail?productCode=u" + productCode
+    header = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+    html = requests.get(url, headers=header, timeout=10)
+    data = html.json()
+    productID = data['resp'][0]['spuInfo']['rows'][0]['name']
     return re.sub(r'\D', '', productID)
 
-#把日本商品網址轉換成商品ID，回傳str
-def convertJPURL(url):
-    productID = re.search(r'\d{6}', url).group(0)    #用re.serach.group回傳找到符合規則的6位數字串
+#把日本商品網址轉換成商品ID，回傳str，在輸入日本商品網址時呼叫
+def convertJPURLToID(productURL):
+    productID = re.search(r'\d{6}', productURL).group(0)    #用re.serach.group回傳找到符合規則的6位數字串
     return productID
+
+#把商品ID轉換成台灣商品網址，回傳str，在需要查詢台灣商品網址時使用
+def convertTWIDToURL(productID):
+    try:
+        url = 'https://d.uniqlo.com/tw/p/search/products/by-description'
+        headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+        json = {
+            'url':'/tw/zh_TW/search.html?description=' + productID,
+            'pageInfo':{'page':1,'pageSize':24},
+            'belongTo':'pc',
+            'rank':'overall',
+            'priceRange':{'low':0,'high':0},
+            'color':[],
+            'size':[],
+            'identity':[],
+            'exist':[],
+            'searchFlag':True,
+            'description':productID,
+            'stockFilter':'warehouse'
+        }
+        html = requests.post(url, headers=headers, json=json, timeout=10)
+        data = html.json()
+        productCode = data["resp"][0]["productList"][0]["productCode"]
+        return 'https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=' + productCode
+    except:
+        return -1   #找不到商品回傳-1
+
+#把商品ID轉換成日本商品網址，回傳str，在需要查詢日本商品網址時使用
+def convertJPIDToURL(productID):
+    return 'https://www.uniqlo.com/jp/ja/products/E' + productID
 
 client.run(DISCORDTOKEN)
